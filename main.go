b3b2c1756a777411082b9e797272961674be7d7e
JKH <jkhndwrk@gmail.com>
2025-06-22 08:01:48 -0500

    Renamed Encrypt function and added write funtionality

--- main.go
+++ main.go
@@ -4,17 +4,37 @@ func main(){
 
 }
 
-func encryptAndSave(){
+func Encrypt(apiKey []byte, password string, outputFile string){
+  salt := make([]byte, 16)
+  _ := rand.Read(salt)
+
+  key := pbkdf2.Key([]byte(pasword), salt, 100_100, 32, sha256.New)
+  block := aes.NewCipher(key)
+
+  aes.GCM := cipher.NewGCM(block)
+
+  nonce := make([]byte, aesGCM.NonceSize())
+
+  ciphertext := aesGCM.Seal(nil, nonce, bytes.TrimSpace(apiKey), nil)
+
+  f := os.Create(outputFile)
+
+  defer f.close()
+
+  f.Write(salt)
+  f.Write(nonce)
+  f.Write(ciphertext)
+
+  return nil
+
+}
+
+func Decrypt(){
 
 
 }
 
-func decryptFile(){
-
-
-}
-
-func check(){
+func ErrorCheck(){
 
 
 }
